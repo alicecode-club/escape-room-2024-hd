6357e5fcd66c7bfc05161a6ac5319251be8e326d
DaniellaJonas <daniella.jonas11@gmail.com>
2024-02-22 19:21:18 +0200

    genie settings

--- aladdin/aladdin.js
+++ aladdin/aladdin.js
@@ -2,23 +2,22 @@ function showMessage(){
     document.getElementById("lamp").setAttribute("onclick", null)
     document.getElementById("lamp").style.cursor="auto";
     window.alert("You will be asked two questions, every time you'll answer a question currectly you would get closer to saving Aladdin from Jafar.")
-    // window.prompt("When Aladdin promises to free him, which Disney character does Genie turn into?");
-    // let answer1;
-    // let firstQuest = prompt("When Aladdin promises to free him, which Disney character does Genie turn into?");
     let correct = false;
-    // let answer2;
     let correct2 = false;
+    var genie = document.getElementById("genie");
+
 
 
     while (correct==false){
         let firstQuest = prompt("When Aladdin promises to free him, which Disney character does Genie turn into?");
         if (firstQuest === "Pinocchio") {
+            genie.style.display = "block";
             window.alert("Well Done!")
             correct = true;
             while (correct2==false){
                 let secondQuest = prompt("How many golden camels does prince Ali have?");
                 if (secondQuest === "75") {
-                    window.alert("You solved the riddle!")
+                    window.alert("You solved the first riddle! Click on the genie for the next one!")
                     correct2 = true;
                 }      
             }
@@ -27,4 +26,5 @@ function showMessage(){
 
 }
 }
+
 
