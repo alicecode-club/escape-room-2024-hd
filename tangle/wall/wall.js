662852278a628bab0ef6f2d44f25a377e7f7cff7
Eliyaluffluff <rosmaneliyah@gmail.com>
2024-02-29 19:40:35 +0200

    rg

--- tangle/wall/wall.js
+++ tangle/wall/wall.js
@@ -1,4 +1,25 @@
+var arrow = document.getElementById("arrow")
+
 function ArrowClick(){
+    arrow
     var div = document.getElementById("game-board");
     div.innerHTML = ``
 }
+
+function ArrowClick2(){
+
+}
+function ArrowClick3(){
+
+}
+function ArrowClick4(){
+
+}
+function ArrowClick5(){
+
+}
+
+function ArrowClick6(){
+
+}
+document.getElementById("thingid").style.visibility="hidden";
