e9e1e2224ad81ac7b64c4fdabe4c7049021bd14e
Yaaritazran09 <yaarazran09@gmail.com>
2024-03-04 20:07:29 +0200

    js

--- beauty_and_the_beast/batb2.js
+++ beauty_and_the_beast/batb2.js
@@ -6,6 +6,15 @@ var teapot2 = 0
 var lumiere2 = 0
 
 function checkteacup(id){
+    if (id=="chip1"){
+        teacup1 = 1
+    }
+    if (id=="chip2"){
+        teacup2 = 1
+    }
+    if (teacup1 == 1 && teacup2 == 1){
+        alert("teacup")
+    }
     console.log(id)
 }
 
