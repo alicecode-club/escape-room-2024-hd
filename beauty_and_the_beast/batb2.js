40311996a5db75c14c19c4676958de1cd60861a3
Yaaritazran09 <yaarazran09@gmail.com>
2024-03-07 19:30:06 +0200

    js

--- beauty_and_the_beast/batb2.js
+++ beauty_and_the_beast/batb2.js
@@ -14,14 +14,35 @@ function checkteacup(id){
     }
     if (teacup1 == 1 && teacup2 == 1){
         alert("teacup")
+        // Who is the only princess that is based on a real life person? Pocahontas
     }
     console.log(id)
 }
 
 function checklumiere(id){
+    if (id=="lumiere1"){
+        lumiere1 = 1
+    }
+    if (id=="lumiere2"){
+        lumiere2 = 1
+    }
+    if (lumiere1 == 1 && lumiere2 == 1){
+        alert("candle")
+        // What was the first animated full-length film? Snow White and the Seven Dwarfs
+    }
     console.log(id)
 }
 
 function checkteapot(id){
+    if (id=="teapot1"){
+        teapot1 = 1
+    }
+    if (id=="teapot2"){
+        teapot2 = 1
+    }
+    if (teapot1 == 1 && teapot2 == 1){
+        alert("teapot")
+        // When is Walt Disney's birth date? December 5 1901
+    }
     console.log(id)
 }
