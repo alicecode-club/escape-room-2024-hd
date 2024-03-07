04552f4626c4a773319887727b086fe9589410c3
DaniellaJonas <daniella.jonas11@gmail.com>
2024-03-07 19:27:33 +0200

    finishing room 3

--- aladdin/room3/aladdinRoomThree.js
+++ aladdin/room3/aladdinRoomThree.js
@@ -1,15 +1,48 @@
 function bossLevel(){
     document.getElementById("anj").setAttribute("onclick", null);
     document.getElementById("anj").style.cursor="auto";
+    let time = 0;
+
     alert("This is your final questions! You will have only three tries and if you fail, aladdin will remain mine!")
 
-    let bossQuest = prompt("How many camels does Prince Ali, or should I say Aladdin, have?");
-    if (bossQuest === "75"){
-        alert("You made it! Aladdin is free!")
+    function question(time){
+        if (time===2){
+            alert("here's a clue: the range is between 65 to 80")
+        }
+        if (time===3){
+            alert("I'm sorry, but you did not manage to free aladdin")
+            return -1
+        }
     }
 
-    else {
+    while (true){
         let bossQuest = prompt("How many camels does Prince Ali, or should I say Aladdin, have?");
-
+        if (bossQuest === "75"){
+            alert("You made it! Aladdin is free!")
+            break
+        }
+    
+        else {
+            time=time+1;
+            console.log(time)
+            var a= question(time)
+            if (a===-1){
+                break
+            }
+        }
+    
     }
-}
+    }
+    // let bossQuest = prompt("How many camels does Prince Ali, or should I say Aladdin, have?");
+    //     if (bossQuest === "75"){
+    //         alert("You made it! Aladdin is free!")
+    //     }
+    
+    //     else {
+    //         time=time+1;
+    //         console.log(time)
+    //         question(time)
+    //     }
+    
+    // }
+
